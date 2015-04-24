2943ba97568305ca344e9e3fa3a4b662df60a089
filipeherculano <filipeherculanorocha@gmail.com>
2015-04-24 01:28:50 -0300

    testes e funcinalidades novas ativadas. metodos calcularSaldo funcionando assim como alugarLivro com suas datas sendo geradas.

--- trabalhoFinal/src/codigoFonte/Acervo.java
+++ trabalhoFinal/src/codigoFonte/Acervo.java
@@ -1,22 +1,17 @@
-package codigoFonte;
-
 /** This code is open-source. Every item inside it is free to use wherever you want as long as you don't sell it.
  * Keep this code open-source and share it! 
  * This code was built as a conclusion program for a OOP(Objects-Oriented Programming) class. 
  * @author Filipe Herculano Rocha & Gabriel Angelo Freire Gonçalves
  */
+package codigoFonte;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.List;
 import java.util.Random;
 import java.util.logging.Level;
@@ -27,6 +22,7 @@ import org.jdom2.Element;
 import org.jdom2.JDOMException;
 import org.jdom2.input.SAXBuilder;
 import org.jdom2.output.XMLOutputter;
+import org.joda.time.LocalDate;
 
 public class Acervo{
     private ArrayList<Livro> livros = new ArrayList();
@@ -42,8 +38,11 @@ public class Acervo{
         Element root = null, user;
         Attribute userEntrega = null, userAluguel = null;
         
-        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-        Calendar calendar = null;
+        LocalDate now = LocalDate.now(), next = now.plusDays(7);
+        String currentDate = now.toString(), nextDate = next.toString();
+        
+        l.setAluguel(currentDate);
+        l.setEntrega(nextDate);
         
         if(file.exists()){
                 SAXBuilder builder = new SAXBuilder();
@@ -58,21 +57,18 @@ public class Acervo{
             
             newDocument = new Document(root);
         }
-        
         List<Element> listEditora = root.getChildren();
         for(Element e : listEditora){
-            if(e.getAttribute("nome").equals(l.getEditora())){
+            if(e.getAttributeValue("nome").equals(l.getEditora())){
                 List<Element> listLivro = e.getChildren();
                 for(Element b : listLivro){
-                    if(Integer.parseInt(b.getAttributeValue("disponível")) > 0 && b.getText().equals(l.getTitulo())){
+                    if(Integer.parseInt(b.getAttributeValue("disponível")) > 0 && b.getAttributeValue("título").equals(l.getTitulo())){
                         int aux = Integer.parseInt(b.getAttributeValue("disponível")) - 1;
                         
-                        Attribute d = new Attribute("disponível", String.valueOf(aux));
-                        b.setAttribute(d);
+                        b.getAttribute("disponível").setValue(String.valueOf(aux));
                         
-                        userAluguel = new Attribute("aluguel", dateFormat.format(calendar.getTime()));
-                        calendar.add(Calendar.DATE, 15);
-                        userEntrega = new Attribute("entrega", dateFormat.format(calendar.getTime()));
+                        userAluguel = new Attribute("dataAluguel", currentDate);
+                        userEntrega = new Attribute("dataEntrega", nextDate);
                         
                         user = new Element("user");
                         user.setAttribute("tipo", u.getTipo());
@@ -120,16 +116,15 @@ public class Acervo{
                     if(e.getAttribute("nome").equals(l.getEditora())){
                         List<Element> listLivro = e.getChildren();
                         for(Element b : listLivro){
-                            if(b.getText().equals(l.getTitulo())){
+                            if(b.getAttributeValue("título").equals(l.getTitulo())){
                                 int aux = Integer.parseInt(b.getAttributeValue("disponível")) + 1;
                                 
-                                Attribute d = new Attribute("disponível", String.valueOf(aux));
-                                b.setAttribute(d);
+                                b.getAttribute("disponível").setValue(String.valueOf(aux));
                                 
                                 List<Element> listUser = b.getChildren();
                                 for(Element c : listUser){
                                     if(c.getAttributeValue("matrícula").equals(u.getMatricula())){
-                                        
+                                        b.removeContent(c);
                                     }
                                 }
                             }
@@ -151,6 +146,80 @@ public class Acervo{
         }
         
         return success;
+    }
+    
+    //incompleto
+    public Double devolverLivro(User u , Livro l) throws JDOMException{
+        boolean success = false;
+        File file = new File("Sistema.xml");
+        Document newDocument = null;
+        Element root = null, aux = null;
+        boolean bookExists = false;
+        
+        if(file.exists()){
+                SAXBuilder builder = new SAXBuilder();
+            try{
+                newDocument = builder.build(file);
+            } catch (IOException ex) {
+                Logger.getLogger(Acervo.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            root = newDocument.getRootElement();
+        }
+        
+        List<Element> listUser = root.getChildren();
+        for(Element a : listUser){
+            if(a.getAttributeValue("matrícula").equals(u.getMatricula())){
+                List<Element> listLivro = a.getChildren();
+                for(Element b : listLivro){
+                    if(b.getAttributeValue("editora").equals(l.getEditora()) && b.getAttributeValue("autor").equals(l.getAutor()) && b.getAttributeValue("título").equals(l.getTitulo())){
+                        aux = b;
+                        a.removeContent(b);
+                        bookExists = true;
+                    } 
+                }
+            }
+        }
+        
+        XMLOutputter out = new XMLOutputter();
+        
+        try {
+            FileWriter arquivo = new FileWriter(file);
+            out.output(newDocument, arquivo);
+        } catch (IOException ex) {
+            Logger.getLogger(Acervo.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        if(bookExists){
+            file = new File("Acervo.xml");
+        
+            if(file.exists()){
+                    SAXBuilder builder = new SAXBuilder();
+                try{
+                    newDocument = builder.build(file);
+                } catch (IOException ex) {
+                    Logger.getLogger(Acervo.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                root = newDocument.getRootElement();
+            }
+
+            List<Element> listEditora = root.getChildren();
+            for(Element a : listEditora){
+                List<Element> listLivro = a.getChildren();
+                for(Element b : listLivro){
+                    if(b.getAttributeValue("autor").equals(aux.getAttributeValue("autor")) && b.getAttributeValue("título").equals(aux.getAttributeValue("título"))){
+                        String auxiliar = String.valueOf(Integer.parseInt(b.getAttributeValue("disponível"))+1) ;
+                        b.getAttribute("disponível").setValue(auxiliar);
+                    }
+                }
+            }
+            
+        }
+        
+        
+        
+        
+        
+        return 0.0;
     }
     
     public boolean addLivro(Livro l) throws JDOMException{
@@ -182,7 +251,7 @@ public class Acervo{
                 noEditora = false;
                 List<Element> listLivro = e.getChildren();
                 for(Element b : listLivro){
-                    if(b.getText().equals(l.getTitulo()) && b.getAttributeValue("autor").equals(l.getAutor())){
+                    if(b.getAttributeValue("título").equals(l.getTitulo()) && b.getAttributeValue("autor").equals(l.getAutor())){
                         int total = Integer.parseInt(b.getAttributeValue("quantidade"));
                         int disp = Integer.parseInt(b.getAttributeValue("disponível"));
                         total += l.getQuantidade();
@@ -210,7 +279,7 @@ public class Acervo{
                     livro.setAttribute(autor);
                     livro.setAttribute(quantidade);
                     livro.setAttribute(avaible);
-                    livro.setText(l.getTitulo());
+                    livro.setAttribute("título", l.getTitulo());
                     
                     e.addContent(livro);
                     success = true;
@@ -237,7 +306,7 @@ public class Acervo{
             livro.setAttribute(autor);
             livro.setAttribute(quantidade);
             livro.setAttribute(avaible);
-            livro.setText(l.getTitulo());
+            livro.setAttribute("título", l.getTitulo());
                     
             editora.addContent(livro);
             
@@ -284,7 +353,7 @@ public class Acervo{
             if(e.getAttributeValue("nome").equals(l.getEditora())){
                 List<Element> listLivro = e.getChildren();
                 for(Element c : listLivro){
-                    if(c.getText().equals(l.getTitulo()) && c.getAttributeValue("autor").equals(l.getAutor())){
+                    if(c.getAttributeValue("título").equals(l.getTitulo()) && c.getAttributeValue("autor").equals(l.getAutor())){
                         e.removeContent(c);
                         removed = true;
                     }
@@ -329,7 +398,7 @@ public class Acervo{
             if(e.getAttributeValue("nome").equals(l.getEditora())){
                 List<Element> listLivros = e.getChildren();
                 for(Element b : listLivros){
-                    if(b.getAttributeValue("autor").equals(l.getAutor()) && b.getText().equals(l.getTitulo())){
+                    if(b.getAttributeValue("autor").equals(l.getAutor()) && b.getAttributeValue("título").equals(l.getTitulo())){
                         if(b.getAttributeValue("disponível").equals(b.getAttributeValue("quantidade"))){
                             if(Integer.parseInt(b.getAttributeValue("disponível")) == quantidade){
                                 e.removeContent(b);
@@ -398,34 +467,15 @@ public class Acervo{
             for(Element a : listEditora){
                 List<Element> listLivro = a.getChildren();
                 for(Element b : listLivro){
-                    if(b.getText().equals(titulo)){
+                    if(b.getAttributeValue("título").equals(titulo)){
                         l.setAutor(b.getAttributeValue("autor"));
                         l.setEditora(a.getAttributeValue("nome"));
                         l.setId(b.getAttributeValue("id"));
                         l.setQuantidade(Integer.parseInt(b.getAttributeValue("quantidade")));
                         l.setDisponivel(Integer.parseInt(b.getAttributeValue("disponível")));
-                        l.setTitulo(b.getText());
+                        l.setTitulo(b.getAttributeValue("título"));
                         livros.add(l);
                     }
-//                    if(notFound){
-//                        String []livroSplit = b.getText().split("\\s+");
-//                        for(int i = 0; i < tituloSplit.length; i++){
-//                            while(notFound){
-//                                for(int j = 0; j < livroSplit.length; i++){
-//                                    if(tituloSplit[i].equals(livroSplit[j])){
-//                                        l.setAutor(b.getAttributeValue("autor"));
-//                                        l.setEditora(a.getAttributeValue("nome"));
-//                                        l.setQuantidade(Integer.parseInt(b.getAttributeValue("quantidade")));
-//                                        l.setDisponivel(Integer.parseInt(b.getAttributeValue("disponível")));
-//                                        l.setTitulo(b.getText());
-//                                        livros.add(l);
-//
-//                                        notFound = false;
-//                                    }
-//                                }
-//                            }
-//                        }
-//                    }
                 }
             }
         }
@@ -497,7 +547,7 @@ public class Acervo{
                 for(Element b : listLivro){
                     if(b.getAttributeValue("id").equals(l.getId())){
                         b.getAttribute("autor").setValue(l.getAutor());
-                        a.getChild("livro").setText(l.getTitulo());
+                        b.getAttribute("título").setValue(l.getTitulo());
                         success = true;
                         noEditora = false;
                     }
@@ -514,22 +564,19 @@ public class Acervo{
                 for(Element b : listLivro){
                     if(b.getAttributeValue("id").equals(l.getId())){
                         b.getAttribute("autor").setValue(l.getAutor());
-                        a.getChild("livro").setText(l.getTitulo());
+                        b.getAttribute("título").setValue(l.getTitulo());
                         aux = b;
                         listUser = b.getChildren();
                         a.removeContent(b);
                     }
                 }
             }
-            
             //se existir a editora que nomeei.
             for(Element a : listEditora){
                 if(a.getAttributeValue("nome").equals(l.getEditora())){
                     newEditora = false;
                     success = true;
-                    for(Element b : listUser){
-                        aux.addContent(b);
-                    }
+                    
                     a.addContent(aux);
                 }
             }
@@ -541,10 +588,6 @@ public class Acervo{
                 Attribute name = new Attribute("nome", l.getEditora());
                 
                 editora.setAttribute(name);
-                
-                for(Element b : listUser){
-                    aux.addContent(b);
-                }
                 
                 editora.addContent(aux);
                 root.addContent(editora);
